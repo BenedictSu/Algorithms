53af21b6e030b11b1f1390dbbefe95760f107de9
BenedictSu <BenedictTheGreat@hotmail.com>
2018-07-07 17:06:32 +0800

    setup skeleton for Sorting

---
+++ 2.Sorting/Sorting.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int input;
+    while (EOF != scanf("%d", &input)) {
+        printf("%d\n", input);
+    }
+    return 0;
+}
---
+++ 2.Sorting/execute.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -g Sorting.cpp -o Sorting && ./Sorting < input.txt > output.txt && diff expected.txt output.txt
