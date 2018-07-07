baf7bc925aa54521c0b3b6d8c04d03bfae862677
BenedictSu <BenedictTheGreat@hotmail.com>
2018-07-07 18:26:21 +0800

    add check for simple swap
    - handle swap cases where j = i+1

--- 2.Sorting/Sorting.cpp
+++ 2.Sorting/Sorting.cpp
@@ -2,22 +2,47 @@
 
 using namespace std;
 
-int main() {
-    int inputSize;
-    while (EOF != scanf("%d", &inputSize)) {
-        int previousInput, currentInput;
-        int isSorted = 1;
-        // init with the first number
-        scanf("%d", &previousInput);
-        for (int i = 1; i < inputSize; i++) {
-            scanf("%d", &currentInput);
-            if (currentInput < previousInput) {
-                isSorted = 0;
-            }
-            previousInput = currentInput;
+#define MAX_N 100001
+
+int inputSize, inputs[MAX_N];
+// variables for swapCheck
+int swapCount, tempLeftSwap, leftSwap, rightSwap;
+
+void swapCheck(int index) {
+    // only check for swap there were less than 2 swaps done
+    if (swapCount < 2) {
+        if (inputs[index] < inputs[index - 1]) {
+            leftSwap = index - 1;
+            rightSwap = index;
+            inputs[index] = inputs[index - 1];
+            swapCount++;
         }
-        if (isSorted) {
+    }
+}
+
+int main() {
+    while (EOF != scanf("%d", &inputSize)) {
+        
+        int isSorted = 1;
+        // reset variables
+        swapCount = 0, tempLeftSwap = -1;
+
+        // read in all the inputs into inputs array
+        scanf("%d", &inputs[1]);
+        for (int i = 2; i <= inputSize; i++) {
+            scanf("%d", &inputs[i]);
+        }
+
+        // do a simple swap check
+        for (int i = 2; i <= inputSize; i++) {
+            swapCheck(i);
+        }
+
+        if (0 == swapCount) {
             printf("yes\n");
+        } else if (1 == swapCount) {
+            printf("yes\n");
+            printf("swap %d %d\n", leftSwap, rightSwap);
         } else {
             printf("no\n");
         }
--- 2.Sorting/execute.sh
+++ 2.Sorting/execute.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-g++ -g Sorting.cpp -o Sorting && ./Sorting < input.txt > output.txt && diff expected.txt output.txt
+g++ -g Sorting.cpp -o Sorting
+./Sorting < input.txt > output.txt
+diff expected.txt output.txt
