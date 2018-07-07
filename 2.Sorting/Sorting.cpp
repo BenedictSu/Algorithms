c06d021554c0d2524827f6a4c1e8354276323f1d
BenedictSu <BenedictTheGreat@hotmail.com>
2018-07-07 17:25:04 +0800

    Add a simple check for sorted/unsorted inputs

--- 2.Sorting/Sorting.cpp
+++ 2.Sorting/Sorting.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 int main() {
-    int input;
-    while (EOF != scanf("%d", &input)) {
-        printf("%d\n", input);
+    int inputSize;
+    while (EOF != scanf("%d", &inputSize)) {
+        int previousInput, currentInput;
+        int isSorted = 1;
+        // init with the first number
+        scanf("%d", &previousInput);
+        for (int i = 1; i < inputSize; i++) {
+            scanf("%d", &currentInput);
+            if (currentInput < previousInput) {
+                isSorted = 0;
+            }
+            previousInput = currentInput;
+        }
+        if (isSorted) {
+            printf("yes\n");
+        } else {
+            printf("no\n");
+        }
     }
     return 0;
 }
